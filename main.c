f6247a739644711155207a7c8bf4ff574f1d4268
decamun <decamun@gmail.com>
2015-10-31 14:22:40 -0400

    housekeeping / indenting

--- main.c
+++ main.c
@@ -5,21 +5,21 @@
  */
 
 #include <avr/io.h>
- #include "m_general.h"
- #include "m_usb.h"
- #include "m_bus.h"
- #include "m_rf.h"
- #include "m_imu.h"
- #include "timer0.h"
+#include "m_general.h"
+#include "m_usb.h"
+#include "m_bus.h"
+#include "m_rf.h"
+#include "m_imu.h"
+#include "timer0.h"
 #include "timer1.h"
 #include "ADC.h"
 #include <math.h>
 
 #define accel_scale 0
- #define gyro_scale 0
- #define CHANNEL 1
- #define RXADDRESS 0x6B //alto 
- #define PACKET_LENGTH 3
+#define gyro_scale 0
+#define CHANNEL 1
+#define RXADDRESS 0x6B //alto
+#define PACKET_LENGTH 3
 
 //unsigned char = m_imu_init(unsigned char accel_scale, unsigned char gyro_scale);
 
@@ -35,7 +35,7 @@ int main(void)
 		m_clockdivide(0); //16MHz
 		m_usb_init();
 		m_imu_init(accel_scale, gyro_scale);
-		
+
 
     while(1){
 
